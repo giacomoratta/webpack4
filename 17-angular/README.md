608d22f88e0a312ee6956e8d0994a3745f8fac4a
giacomoratta <giacomo.ratta@diymaxeda.com>
2020-10-21 09:41:48 +0200

    17 - init

---
+++ 17-angular/README.md
@@ -0,0 +1,6 @@
+## Webpack4 + Angular
+(cloned from 16-typescript)
+
+Install:
+- `typescript`
+- `awesome-typescript-loader`
---
+++ 17-angular/config/webpack.dev.js
@@ -0,0 +1,105 @@
+const path = require('path')
+const webpack = require('webpack')
+
+// hot-reloading when html changes
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: {
+    main: './src/main.js',
+    ts: './src/index.ts'
+  },
+  devtool: 'source-map',
+  mode: 'development',
+  output: {
+    filename: '[name]-bundle.js',
+    path: path.resolve(__dirname, '../dist'),
+    publicPath: '/'
+  },
+  devServer: {
+    contentBase: 'dist',
+
+    // show error overlay
+    overlay: true,
+
+    // tune webpack output
+    stats: {
+      colors: true
+    },
+
+    // hot reloading
+    hot: true
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          { loader: 'babel-loader' }
+        ]
+      },
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: [
+          { loader: 'awesome-typescript-loader' }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' }
+        ]
+      },
+      {
+        test: /\.html$/,
+        use: [
+
+          // both loaders replaced by HTMLWebpackPlugin which does the same functions
+          // { loader: 'file-loader', options: { name: "[name].html" } },
+          // { loader: 'extract-loader' },
+
+          {
+            // does the linting
+            loader: 'html-loader',
+            options: {
+              attributes: {
+                list: [
+                  // All default supported tags and attributes
+                  '...',
+                  {
+                    tag: 'img',
+                    attribute: 'data-src',
+                    type: 'src'
+                  }
+                ]
+              }
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(jpg|gif|png)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              // hash not required, just example
+              name: 'images/[name]-[hash:8].[ext]'
+            }
+          }
+        ]
+      }
+    ]
+  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(), // needed for hot-reloading
+    new HTMLWebpackPlugin({
+      template: './src/index.html'
+    })
+  ]
+}
