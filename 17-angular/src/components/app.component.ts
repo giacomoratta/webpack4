4c45fcc232982b31e2108f034a9a2041b4500b98
giacomoratta <giacomo.ratta@diymaxeda.com>
2020-10-23 07:21:46 +0200

    17 (end) - bind input field and add css to component

--- 17-angular/src/components/app.component.ts
+++ 17-angular/src/components/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core'
 
 @Component({
     selector: 'root-app',
-    styles: [],
+    styles: [require('./app.component.css').toString()],
     template: require('./app.component.html')
 })
 
