0a83014774577fd911dbe5e2a65203147e07e936
giacomoratta <giacomo.ratta@diymaxeda.com>
2020-10-23 07:03:34 +0200

    17 - set angular components

---
+++ 17-angular/src/components/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { AppRoot } from './app.component'
+
+@NgModule({
+    bootstrap: [AppRoot],
+    declarations: [AppRoot],
+    imports: [BrowserModule]
+})
+
+export class AppModule {
+
+}
