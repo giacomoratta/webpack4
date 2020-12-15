cd67256de1f0fa0303f5eeb6f8fbdb0dac6cc21c
giacomoratta <giacomo.ratta@diymaxeda.com>
2020-12-15 11:26:17 +0100

    19 - first local run (prod), Procfile, env

--- 19-heroku/src/server/express.js
+++ 19-heroku/src/server/express.js
@@ -2,28 +2,37 @@ import express from 'express'
 import path from 'path'
 const server = express()
 
-const webpack = require('webpack')
-const config = require('../../config/webpack.dev.js')
-const compiler = webpack(config)
+const isProd = process.env.NODE_ENV === 'production'
 
-const webpackDevMiddleware = require('webpack-dev-middleware')(
-  compiler,
-  config.devServer
-)
+// Webpack middleware only in DEV mode!
+if (!isProd) {
+  const webpack = require('webpack')
+  const config = require('../../config/webpack.dev.js')
+  const compiler = webpack(config)
 
-const webpackHotMiddlware = require('webpack-hot-middleware')(
-  compiler,
-  config.devServer
-)
+  const webpackDevMiddleware = require('webpack-dev-middleware')(
+    compiler,
+    config.devServer
+  )
 
-server.use(webpackDevMiddleware)
-server.use(webpackHotMiddlware)
-console.log('Middleware enabled')
+  const webpackHotMiddlware = require('webpack-hot-middleware')(
+    compiler,
+    config.devServer
+  )
 
+  server.use(webpackDevMiddleware)
+  server.use(webpackHotMiddlware)
+  console.log('Middleware enabled')
+}
+
+// In production, heroku will use only files in dist directory
+// ...but we need to build them first: npm run build
 const staticMiddleware = express.static('dist')
 server.use(staticMiddleware)
 
+// env.PORT set by heroku
 const PORT = process.env.PORT || 8080
+
 server.listen(PORT, () => {
   console.log(`Server listening on http://localhost:${PORT}`)
 })
