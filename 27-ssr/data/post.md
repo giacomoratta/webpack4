b25cf736219ddd19e79651bcee92c3ea93cfff6a
giacomoratta <giacomo.ratta@diymaxeda.com>
2020-12-21 22:01:22 +0100

    27 - init project from 19-heroku

---
+++ 27-ssr/data/post.md
@@ -0,0 +1,9 @@
+---
+title: Our first Post
+author: Link
+---
+
+# Long ago in the Kingdom of Hyrule
+
+Our hero wakes up.
+He is alone.
---
+++ 27-ssr/src/server/express.js
@@ -0,0 +1,42 @@
+import express from 'express'
+import path from 'path'
+const server = express()
+
+const isProd = process.env.NODE_ENV === 'production'
+
+// Webpack middleware only in DEV mode!
+if (!isProd) {
+  const webpack = require('webpack')
+  const config = require('../../config/webpack.dev.js')
+  const compiler = webpack(config)
+
+  const webpackDevMiddleware = require('webpack-dev-middleware')(
+    compiler,
+    config.devServer
+  )
+
+  const webpackHotMiddlware = require('webpack-hot-middleware')(
+    compiler,
+    config.devServer
+  )
+
+  server.use(webpackDevMiddleware)
+  server.use(webpackHotMiddlware)
+  console.log('Middleware enabled')
+}
+
+// In production, heroku will use only files in dist directory
+// ...but we need to build them first: npm run build
+// (replaced with gzip support) const staticMiddleware = express.static('dist')
+// (replaced with gzip support) server.use(staticMiddleware)
+const expressStaticGzip = require('express-static-gzip')
+server.use(expressStaticGzip('dist', {
+  enableBrotli: true
+}))
+
+// env.PORT set by heroku
+const PORT = process.env.PORT || 8080
+
+server.listen(PORT, () => {
+  console.log(`Server listening on http://localhost:${PORT}`)
+})
