aee227855de65f9293c7b0181153beb9cbcf17c9
giacomoratta <giacomo.ratta@diymaxeda.com>
2020-12-22 09:54:56 +0100

    29 - init project from project 27

---
+++ 29-unicomp/config/webpack.dev.js
@@ -0,0 +1,118 @@
+const path = require('path')
+const webpack = require('webpack')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+
+module.exports = {
+  entry: {
+    main: [
+      'react-hot-loader/patch',
+      'babel-runtime/regenerator',
+      'webpack-hot-middleware/client?reload=true',
+      './src/main.js'
+    ],
+    other: [
+      'react-hot-loader/patch',
+      'babel-runtime/regenerator',
+      'webpack-hot-middleware/client?reload=true',
+      './src/main.js'
+    ]
+  },
+  mode: 'development',
+  output: {
+    filename: '[name]-bundle.js',
+    path: path.resolve(__dirname, '../dist'),
+    publicPath: '/'
+  },
+  devServer: {
+    contentBase: 'dist',
+    overlay: true,
+    stats: {
+      colors: true
+    }
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      cacheGroups: {
+        vendor: {
+          name: 'vendor',
+          chunks: 'initial',
+          minChunks: 2
+        }
+
+      }
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader'
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          }
+        ]
+      },
+      {
+        test: /\.jpg$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'images/[name].[ext]'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.md$/,
+        use: [
+          // {
+          //   loader: 'html-loader'
+          // },
+          // {
+          //   loader: 'markdown-loader'
+          // }
+          {
+            loader: 'markdown-with-front-matter-loader'
+          }
+        ]
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader'
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('development'),
+        CUSTOM_VAR1: JSON.stringify('value1-dev')
+      }
+    }),
+    // new HTMLWebpackPlugin({
+    //   template: './src/index.ejs',
+    //   inject: true,
+    //   title: "Link's Journal"
+    // })
+  ]
+}
