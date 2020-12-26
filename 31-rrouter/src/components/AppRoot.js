5023055f0dbcefc535a3e1bfb8ca343e13ecfb54
giacomoratta <giacomo.ratta@diymaxeda.com>
2020-12-26 08:39:03 +0100

    31 - added basic routing

--- 31-rrouter/src/components/AppRoot.js
+++ 31-rrouter/src/components/AppRoot.js
@@ -1,32 +1,49 @@
 import React from 'react'
 import MarkdownData from '../../data/post.md'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import './nav.css'
+
+const About = () => (
+  <div className='profile'>
+    <img src={require('../images/link.jpg')} />
+    <h1>{MarkdownData.title}</h1>
+    <h2>{MarkdownData.author}</h2>
+    <div className='content' dangerouslySetInnerHTML={{ __html: MarkdownData.__content }} />
+  </div>
+)
+
+const Gallery = () => (
+  <div>
+    <h1>Gallery</h1>
+  </div>
+)
+
+const Article = () => (
+  <div>
+    <h1>Article</h1>
+  </div>
+)
 
 export default class extends React.Component {
   constructor (props) {
     super(props)
-    this.state = {
-      count: 0
-    }
+    this.state = { }
   }
 
   render () {
     return (
-      <div className='profile'>
-        <img src={require('../images/link.jpg')} />
-        <h1>{MarkdownData.title}</h1>
-        <h2>{MarkdownData.author}</h2>
-        <div className='content' dangerouslySetInnerHTML={{ __html: MarkdownData.__content }} />
-      </div>
-    )
-  }
-
-  render1 () {
-    return (
-      <div className='profile'>
-        <img src={""} />
-        <h1>{"Title"}</h1>
-        <div className='content' dangerouslySetInnerHTML={{ __html: "<h1>Heading</h1>" }} />
-      </div>
+      <Router>
+        <div>
+          <div className="nav">
+            <Link to="/">Gallery</Link>
+            <Link to="/about">About</Link>
+            <Link to="/article">Article</Link>
+          </div>
+          <Route exact path="/" component={Gallery} />
+          <Route path="/about" component={About} />
+          <Route path="/article" component={Article} />
+        </div>
+      </Router>
     )
   }
 }
