a6fec4c962b4a782ff43a3cd0aded59aac9dbb66
giacomoratta <giacomo.ratta@diymaxeda.com>
2020-12-30 09:37:01 +0100

    35 - (39) setup redux for server

--- 35-multidomains/src/server/render.js
+++ 35-multidomains/src/server/render.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { renderToString } from 'react-dom/server'
 import { StaticRouter } from 'react-router'
 import Routes from '../components/Routes'
+import { Provider } from 'react-redux'
+import store from '../store'
 
 import { flushChunkNames } from 'react-universal-component/server'
 import flushChunks from 'webpack-flush-chunks'
@@ -12,9 +14,11 @@ export default ({ clientStats }) => (req, res) => {
   }
 
   const app = renderToString(
-    <StaticRouter location={req.originalUrl} context={context}>
-      <Routes />
-    </StaticRouter>
+    <Provider store={store}>
+      <StaticRouter location={req.originalUrl} context={context}>
+        <Routes />
+      </StaticRouter>
+    </Provider>
   )
 
   const names = flushChunkNames().concat([`css/${context.site}-theme-css`])
